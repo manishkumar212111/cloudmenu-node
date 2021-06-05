bfce6b2e442d2b9fb7973ae70b04ef970d17a47f
Manish Kumar <manish.k@darkbears.com>
2021-06-05 21:58:09 +0530

    lab issue quick fix

--- src/services/stripe.service.js
+++ src/services/stripe.service.js
@@ -1,18 +1,21 @@
 var axios = require('axios');
 
+const auth_token = 'Bearer b39bccad2e0e0c14b7dd511522fecc2b0e98b76111f23f74ab08dccead84a1a325ded1efa55143391576ae013476507ffb86a1f91c69eabf8c1912ef1dce7981rv4traaay1kruEjAMWNCkEVtvFH4S+7+kHnr06ubUx03V9OqgEFI4f34MK5E3unIVIzii52rd/1DYkrl/hQktg==';
+// const auth_token = auth_token;
 const createAccount = async (data, cb) => {
 
     var config = {
         method: 'post',
         url: 'https://payment.superfruit.app/accounts/create',
         headers: {
-          'Authorization': 'Bearer 800aa16fb9a949c2c927ff93e0331192feb122fe7a6bb1949fb24d6eabda11e89998205bb448e3ea4559bdbbc5a5c65eeac3f0090cba29fb1243599009ad28dbmOQKu2IJufsEK/zIr359VXG0R5FgJS3GHSkG4/NJ4KKJt/quAoA/hEYoeWolA98Pr33G2ZF3X1NG6wpmUP4odw=='
+          'Authorization': auth_token
         },
         data : data
       };
       
       axios(config)
       .then(function (response) {
+        console.log(response.data);
         cb(response);
       })
       .catch(function (error) {
@@ -29,13 +32,14 @@ const deleteAccount = async (data, cb) => {
         method: 'post',
         url: 'https://payment.superfruit.app/accounts/delete',
         headers: {
-          'Authorization': 'Bearer 800aa16fb9a949c2c927ff93e0331192feb122fe7a6bb1949fb24d6eabda11e89998205bb448e3ea4559bdbbc5a5c65eeac3f0090cba29fb1243599009ad28dbmOQKu2IJufsEK/zIr359VXG0R5FgJS3GHSkG4/NJ4KKJt/quAoA/hEYoeWolA98Pr33G2ZF3X1NG6wpmUP4odw=='
+          'Authorization': auth_token
         },
         data : data
       };
       
       axios(config)
       .then(function (response) {
+        console.log(response.data);
         cb(response);
       })
       .catch(function (error) {
@@ -52,13 +56,14 @@ const createProduct = async (data, cb) => {
         method: 'post',
         url: 'https://payment.superfruit.app/products/create',
         headers: {
-          'Authorization': 'Bearer 800aa16fb9a949c2c927ff93e0331192feb122fe7a6bb1949fb24d6eabda11e89998205bb448e3ea4559bdbbc5a5c65eeac3f0090cba29fb1243599009ad28dbmOQKu2IJufsEK/zIr359VXG0R5FgJS3GHSkG4/NJ4KKJt/quAoA/hEYoeWolA98Pr33G2ZF3X1NG6wpmUP4odw=='
+          'Authorization': auth_token
         },
         data : data
       };
       
       axios(config)
       .then(function (response) {
+        console.log(response.data);
         cb(response);
       })
       .catch(function (error) {
@@ -75,13 +80,14 @@ const bulkCreate = async (data, cb) => {
         method: 'post',
         url: 'https://payment.superfruit.app/products/bulk-create',
         headers: {
-          'Authorization': 'Bearer 800aa16fb9a949c2c927ff93e0331192feb122fe7a6bb1949fb24d6eabda11e89998205bb448e3ea4559bdbbc5a5c65eeac3f0090cba29fb1243599009ad28dbmOQKu2IJufsEK/zIr359VXG0R5FgJS3GHSkG4/NJ4KKJt/quAoA/hEYoeWolA98Pr33G2ZF3X1NG6wpmUP4odw=='
+          'Authorization': auth_token
         },
         data : data
       };
       
       axios(config)
       .then(function (response) {
+        console.log(response.data);
         cb(response);
       })
       .catch(function (error) {
@@ -98,13 +104,14 @@ const deleteProduct = async (data, cb) => {
         method: 'post',
         url: 'https://payment.superfruit.app/products/bulk-create',
         headers: {
-          'Authorization': 'Bearer 800aa16fb9a949c2c927ff93e0331192feb122fe7a6bb1949fb24d6eabda11e89998205bb448e3ea4559bdbbc5a5c65eeac3f0090cba29fb1243599009ad28dbmOQKu2IJufsEK/zIr359VXG0R5FgJS3GHSkG4/NJ4KKJt/quAoA/hEYoeWolA98Pr33G2ZF3X1NG6wpmUP4odw=='
+          'Authorization': auth_token
         },
         data : data
       };
       
       axios(config)
       .then(function (response) {
+        console.log(response.data);
         cb(response);
       })
       .catch(function (error) {
@@ -121,13 +128,14 @@ const createPaymentLink = async (data, cb) => {
         method: 'post',
         url: 'https://payment.superfruit.app/payment-links/create',
         headers: {
-          'Authorization': 'Bearer 800aa16fb9a949c2c927ff93e0331192feb122fe7a6bb1949fb24d6eabda11e89998205bb448e3ea4559bdbbc5a5c65eeac3f0090cba29fb1243599009ad28dbmOQKu2IJufsEK/zIr359VXG0R5FgJS3GHSkG4/NJ4KKJt/quAoA/hEYoeWolA98Pr33G2ZF3X1NG6wpmUP4odw=='
+          'Authorization': auth_token
         },
         data : data
       };
       
       axios(config)
       .then(function (response) {
+        console.log(response.data);
         cb(response);
       })
       .catch(function (error) {
