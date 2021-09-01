397af0eaaff26a5f3a7f5addcb2c0982c83fcaf4
Manish Kumar <manish.k@darkbears.com>
2021-09-01 22:57:59 +0530

    favicon updated

--- src/routes/v1/common.route.js
+++ src/routes/v1/common.route.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const validate = require('../../middlewares/validate');
 const enquiryValidation = require('../../validations/enquiry.validation');
+const productValidation = require('../../validations/product.validation');
+
 const enquiryController = require('../../controllers/enquiry.controller');
+const productController = require('../../controllers/product.controller');
+
+const categoryValidation = require('../../validations/category.validation');
+const categoryController = require('../../controllers/category.controller');
+
+const restaurantValidation = require('../../validations/restaurant.validation');
+const restaurantController = require('../../controllers/restaurant.controller');
 
 const router = express.Router();
 
 router.post('/enquiry', validate(enquiryValidation.createEnquiry), enquiryController.createEnquiry);
+router.get('/product', validate(productValidation.getProducts), productController.getProducts);
+router.get('/category', validate(categoryValidation.getCategorys), categoryController.getCategorys);
+router.get('/restaurant/:restaurantId', validate(restaurantValidation.getRestaurant), restaurantController.getRestaurant);
 
 module.exports = router;
